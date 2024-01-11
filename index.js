29c1bb283202b3975420ee9dad965282a5bb34eb
Drew Benson <Drewbenson16@gmail.com>
2024-01-11 10:48:12 -0700

    yeah

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    // First, we check if the string is empty
+    if (string.length === 0) {
+      return string;
+    }
+
+    // Capitalize the first letter of the string
+    const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
+
+    return capitalizedString;
+  }
+
+  static sanitize(string) {
+    const sanitizedString = string.replace(/[^a-zA-Z0-9-' ]/g, '');
+
+    return sanitizedString;
+  }
+
+  static titleize(string) {
+    const words = string.split(" ");
+
+    const titleizedWords = words.map((word, index) => {
+      if (
+        index === 0 || // Always capitalize the first word
+        !["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"].includes(word)
+      ) {
+        return this.capitalize(word);
+      } else {
+        return word.toLowerCase();
+      }
+    });
+
+    const titleizedString = titleizedWords.join(" ");
+
+    return titleizedString;
+  }
 }
